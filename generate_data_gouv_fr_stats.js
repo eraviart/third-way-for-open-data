db908880d3d7e0c2091fd6da186b8b516d1a9aca
Emmanuel Raviart <emmanuel@raviart.com>
2018-09-26 01:14:51 +0200

    Add data.gouv.fr stats for users.

--- generate_data_gouv_fr_stats.js
+++ generate_data_gouv_fr_stats.js
@@ -2,12 +2,24 @@ const fs = require("fs")
 const path = require("path")
 
 const dataDir = "data.gouv.fr"
-const datasets = JSON.parse(fs.readFileSync(path.join(dataDir, "datasets.json"), { encoding: "utf-8" }))
 
-const countByDate = {}
-for (let dataset of datasets) {
-  if (dataset.deleted) continue
-  const date = dataset.created_at.split("T")[0]
-  countByDate[date] = (countByDate[date] || 0) + 1
+{
+  const countByDate = {}
+  const datasets = JSON.parse(fs.readFileSync(path.join(dataDir, "datasets.json"), { encoding: "utf-8" }))
+  for (let dataset of datasets) {
+    if (dataset.deleted) continue
+    const date = dataset.created_at.split("T")[0]
+    countByDate[date] = (countByDate[date] || 0) + 1
+  }
+  fs.writeFileSync(path.join(dataDir, "datasets-count-by-creation-date.json"), JSON.stringify(countByDate, null, 2))
 }
-fs.writeFileSync(path.join(dataDir, "datasets-count-by-creation-date.json"), JSON.stringify(countByDate, null, 2))
+
+{
+  const countByDate = {}
+  const users = JSON.parse(fs.readFileSync(path.join(dataDir, "users.json"), { encoding: "utf-8" }))
+  for (let user of users) {
+    const date = user.since.split("T")[0]
+    countByDate[date] = (countByDate[date] || 0) + 1
+  }
+  fs.writeFileSync(path.join(dataDir, "users-count-by-creation-date.json"), JSON.stringify(countByDate, null, 2))
+}
