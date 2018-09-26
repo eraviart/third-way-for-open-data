70069ed69af886cf0db601bd5f28247fd9575f29
Emmanuel Raviart <emmanuel@raviart.com>
2018-09-26 02:10:05 +0200

    Add data.gouv.fr stats for discussions, issues & reuses.

--- generate_data_gouv_fr_stats.js
+++ generate_data_gouv_fr_stats.js
@@ -17,6 +17,43 @@ const dataDir = "data.gouv.fr"
 }
 
 {
+  const filename = "discussions-count-by-creation-date.json"
+  console.log(`Generating ${filename}...`)
+  const countByDate = {}
+  const discussions = JSON.parse(fs.readFileSync(path.join(dataDir, "discussions.json"), { encoding: "utf-8" }))
+  for (let discussion of discussions) {
+    const date = discussion.created.split("T")[0]
+    countByDate[date] = (countByDate[date] || 0) + 1
+  }
+  fs.writeFileSync(path.join(dataDir, filename), JSON.stringify(countByDate, null, 2))
+}
+
+{
+  const filename = "issues-count-by-close-date.json"
+  console.log(`Generating ${filename}...`)
+  const countByDate = {}
+  const issues = JSON.parse(fs.readFileSync(path.join(dataDir, "issues.json"), { encoding: "utf-8" }))
+  for (let issue of issues) {
+    if (!issue.closed) continue
+    const date = issue.closed.split("T")[0]
+    countByDate[date] = (countByDate[date] || 0) + 1
+  }
+  fs.writeFileSync(path.join(dataDir, filename), JSON.stringify(countByDate, null, 2))
+}
+
+{
+  const filename = "issues-count-by-creation-date.json"
+  console.log(`Generating ${filename}...`)
+  const countByDate = {}
+  const issues = JSON.parse(fs.readFileSync(path.join(dataDir, "issues.json"), { encoding: "utf-8" }))
+  for (let issue of issues) {
+    const date = issue.created.split("T")[0]
+    countByDate[date] = (countByDate[date] || 0) + 1
+  }
+  fs.writeFileSync(path.join(dataDir, filename), JSON.stringify(countByDate, null, 2))
+}
+
+{
   const filename = "organizations-count-by-creation-date.json"
   console.log(`Generating ${filename}...`)
   const countByDate = {}
@@ -24,6 +61,19 @@ const dataDir = "data.gouv.fr"
   for (let organization of organizations) {
     // if (organization.deleted) continue
     const date = organization.created_at.split("T")[0]
+    countByDate[date] = (countByDate[date] || 0) + 1
+  }
+  fs.writeFileSync(path.join(dataDir, filename), JSON.stringify(countByDate, null, 2))
+}
+
+{
+  const filename = "reuses-count-by-creation-date.json"
+  console.log(`Generating ${filename}...`)
+  const countByDate = {}
+  const reuses = JSON.parse(fs.readFileSync(path.join(dataDir, "reuses.json"), { encoding: "utf-8" }))
+  for (let reuse of reuses) {
+    // if (reuse.deleted) continue
+    const date = reuse.created_at.split("T")[0]
     countByDate[date] = (countByDate[date] || 0) + 1
   }
   fs.writeFileSync(path.join(dataDir, filename), JSON.stringify(countByDate, null, 2))
