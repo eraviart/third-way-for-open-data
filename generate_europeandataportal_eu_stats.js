4352333fc4cb19495b02ed21ff6a3bf12d41bbea
Emmanuel Raviart <emmanuel@raviart.com>
2018-10-02 18:33:47 +0200

    Add stats on modification dates for europendataportal.eu packages.

--- generate_europeandataportal_eu_stats.js
+++ generate_europeandataportal_eu_stats.js
@@ -5,17 +5,27 @@ const path = require("path")
 const dataDir = "europeandataportal.eu"
 
 {
-  const filename = "packages-count-by-creation-date.json"
-  console.log(`Generating ${filename}...`)
+  console.log("Generating packages stats")
   const packagesFilenames = fs.readdirSync(dataDir).filter(filename => /^packages-[0-9]+\.json\.br$/.test(filename))
-  const countByDate = {}
+  const countByMetadataCreationDate = {}
+  const countByMetadataModificationDate = {}
   for (let packageFilename of packagesFilenames) {
     console.log(`  Reading ${packageFilename}...`)
     const datasets = JSON.parse(decompressSync(fs.readFileSync(path.join(dataDir, packageFilename))))
     for (let dataset of Object.values(datasets)) {
-      const date = dataset.metadata_created.split("T")[0]
-      countByDate[date] = (countByDate[date] || 0) + 1
+      const metadataCreationDate = dataset.metadata_created.split("T")[0]
+      countByMetadataCreationDate[metadataCreationDate] = (countByMetadataCreationDate[metadataCreationDate] || 0) + 1
+      const metadataModificationDate = dataset.metadata_created.split("T")[0]
+      countByMetadataModificationDate[metadataModificationDate] =
+        (countByMetadataModificationDate[metadataModificationDate] || 0) + 1
     }
   }
-  fs.writeFileSync(path.join(dataDir, filename), JSON.stringify(countByDate, null, 2))
+  fs.writeFileSync(
+    path.join(dataDir, "packages-count-by-metadata-creation-date.json"),
+    JSON.stringify(countByMetadataCreationDate, null, 2)
+  )
+  fs.writeFileSync(
+    path.join(dataDir, "packages-count-by-metadata-modification-date.json"),
+    JSON.stringify(countByMetadataModificationDate, null, 2)
+  )
 }
